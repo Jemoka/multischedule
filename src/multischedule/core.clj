2b6f6b4b916d190e74b0bae687e9bb1663f961d8
Houjun Liu <hliu@shabang.cf>
2022-05-25 23:47:06 -0700

    adding readme research

--- src/multischedule/core.clj
+++ src/multischedule/core.clj
@@ -2,6 +2,8 @@
   (:require [clojure.core.async :as async])
   (:gen-class))
 
+;; https://personal.utdallas.edu/~ravip/cs6378/slides/node5.html
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
